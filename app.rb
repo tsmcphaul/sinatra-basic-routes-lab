52afba9704624194f48e1edf383176d1d4eeb516
Tammarra McPhaul <shaemc22@gmail.com>
2021-04-05 20:06:14 -0400

    commit tree a2f634303af219cf92b0b1a235c43e1645f6a402

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/name' do
+        "My name is Shae"
+      end 
+
+      get '/hometown' do
+        "My hometown is ATL"
+      end 
+
+      get '/favorite-song' do
+        "My favorite song is Thriller"
+      end 
+
 end
